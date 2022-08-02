78ddb43da10244936956af16862aaa5f485b7268
sruthi-002 <89520373+sruthi-002@users.noreply.github.com>
2022-08-02 10:29:57 +0530

    Update quicksort.java

--- quicksort.java
+++ quicksort.java
@@ -26,12 +26,11 @@ class Quicksort {
 }
 class Main {
   public static void main(String args[]) {
-    int[] data = { 8, 7, 2, 1, 0, 9, 6 };
-    System.out.println("Unsorted Array");
-    System.out.println(Arrays.toString(data));
-    int size = data.length;
-    Quicksort.quickSort(data, 0, size - 1);
-    System.out.println("Sorted Array in Ascending Order ");
-    System.out.println(Arrays.toString(data));
+    int n = in.nextInt();
+    int[] a =new a[n];
+    for(i=0;i<n;i++)
+      a[i] = in.nextInt();
+    Quicksort.quickSort(a, 0, n- 1);
+    System.out.println(Arrays.toString(a));
   }
 }
